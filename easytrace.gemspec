41e72bb9ff49c0355af23434d35dc040c8eeb014
Shikugawa <Shikugawa@gmail.com>
2017-08-31 17:55:16 +0900

    [fix] gemspec

--- easytrace.gemspec
+++ easytrace.gemspec
@@ -9,9 +9,9 @@ Gem::Specification.new do |spec|
   spec.authors       = ["Shikugawa"]
   spec.email         = ["Shikugawa@gmail.com"]
 
-  spec.summary       = %q{TODO: Write a short summary, because Rubygems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.summary       = %q{TracePoint extension library}
+  spec.description   = %q{Use TracePoint more easily }
+  spec.homepage      = "https://github.com/Shikugawa/Easytrace"
   spec.license       = "MIT"
 
   # Prevent pushing this gem to RubyGems.org. To allow pushes either set the 'allowed_push_host'
--- lib/easytrace.rb
+++ lib/easytrace.rb
@@ -1,5 +1,32 @@
 require "easytrace/version"
 
 module Easytrace
-  # Your code goes here...
+    @@DEFINED_METHODS = [:start, :stop, :all_enable, :all_disable].freeze
+    
+    def start(*args, &blk)
+        @trace ||= []
+        blc = block_given? ? Proc.new{ |tr| p tr unless tr.defined_class != 'EasyTrace' && @@DEFINED_METHODS.include?(tr.method_id) } : blk
+        args.each do |opts|
+            @trace << TracePoint.new(opts, &blc)
+        end
+
+        all_enable
+    end
+
+    def stop
+       all_disable
+    end
+
+    private
+    def all_enable
+        @trace.each do |trace_obj|
+            trace_obj.enable unless trace_obj.enabled?
+        end
+    end
+
+    def all_disable
+        @trace.each do |trace_obj|
+            trace_obj.disable if trace_obj.enabled?
+        end
+    end
 end
